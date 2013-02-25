ee214c6c8c46f62b3e42d19883ac2d3af4905959
Denis Renning <denis@devtty.de>
2013-02-25 08:33:06 +0100

    added annotations

--- src/main/java/com/devtty/elab/model/planner/SkillProficiency.java
+++ src/main/java/com/devtty/elab/model/planner/SkillProficiency.java
@@ -1,23 +1,34 @@
 package com.devtty.elab.model.planner;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
 import com.devtty.elab.model.common.AbstractPersistable;
 
+@Entity
 public class SkillProficiency extends AbstractPersistable{
 	
 	private Employee employee;
 	private Skill skill;
+	
+	@ManyToOne
 	public Employee getEmployee() {
 		return employee;
 	}
+	
 	public void setEmployee(Employee employee) {
 		this.employee = employee;
 	}
+	
+	@ManyToOne
 	public Skill getSkill() {
 		return skill;
 	}
+	
 	public void setSkill(Skill skill) {
 		this.skill = skill;
 	}
+	
 	@Override
 	public String toString() {
 		return "SkillProficiency [employee=" + employee + ", skill=" + skill
